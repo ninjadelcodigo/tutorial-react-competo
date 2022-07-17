88b0047aea5ace732c6a924cb4a8a7c56efa581d
Ninja <ninja@Andress-MacBook-Pro.local>
2022-07-17 13:12:06 -0500

    1er commit a leccion-3

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Editar <code>src/App.js</code> y guardar para reload.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://es.reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Aprende React
         </a>
       </header>
     </div>
